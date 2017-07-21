154b0bd73093184885877219b91906346d388fc9
Alex Miranda <alexandermichaelmiranda@gmail.com>
2017-07-21 11:20:30 -0700

    Pushing up working version fulfilling basic reqs

--- Program3/Project3.asm
+++ Program3/Project3.asm
@@ -17,6 +17,8 @@ INCLUDE Irvine32.inc
 LOWER_BOUND = 1
 ; This constant specifies the upper bound for how many composite terms can be displayed
 UPPER_BOUND = 400
+; This constant sets how many composite numbers are displayed per row
+SPACE_COUNT = 10
 
 .data
 
@@ -34,6 +36,10 @@ termPrompt1			BYTE	"Enter the number of composites to display [",0
 termPrompt2			BYTE	" .. ",0
 termPrompt3			BYTE	"]: ",0
 
+; Formatting variables
+
+spaceChar			BYTE	9,0	; variable to hold the whitespace char
+	
 ; Error messaging
 
 rngErrMsg			BYTE	"Out of range.	Try again.",0
@@ -44,12 +50,24 @@ certMsg				BYTE	"Results certified by Alexander Miranda.  Goodbye.",0
 
 ; (insert variable definitions here)
 
-compTerms			DWORD	?	;variable that stores the user inputted desired number of composite terms
+userInput			DWORD	?	; variable that stores the user inputted desired number of composite terms
+userInputTmp		DWORD	?	; additional storage variable for user inputted number, helping with spacing
+compTerm			DWORD	?	; placeholder for composite number to be displayed in loop
+countPH				DWORD	?	; loop counter placeholder variable
+spaceCount			DWORD	?	; variable that holds the space count, initialized with SPACE_COUNT	
 
 .code
 main PROC
 
-introduction:
+	call introduction
+	call getNumOfCompTerms
+	call outputComposites
+	call sayGoodBye
+
+	exit	; exit to operating system
+main ENDP
+
+introduction	PROC
 
 ; Introduction for the user
 	mov			edx, OFFSET programHead
@@ -69,9 +87,14 @@ introduction:
 	call	WriteString
 	call	CrLf
 
+	ret
+introduction ENDP
+
 ; Prompting the user for the number of terms they want to see and storing that number
 
-getNumOfCompTerms:
+getNumOfCompTerms	PROC
+
+grabNum:
 
 	mov			edx, OFFSET termPrompt1
 	call	WriteString
@@ -83,22 +106,44 @@ getNumOfCompTerms:
 	call	WriteDec
 	mov			edx, OFFSET termPrompt3
 	call	WriteString
-	call	readInt
-	mov			compTerms, eax
+	call	ReadInt
+	mov			userInput, eax
+	call	validateInput
+	cmp			ebx, 1
+	je			validNum
+	call	validationError
+	jmp		grabNum
 
-validateInput:
+validNum:
+	ret
+
+getNumOfCompTerms ENDP
+
+validateInput	PROC
 
 	; Checking if the inputted number is zero
 	jz			validationError
 	; Checking if the inputted number is negative
 	js			validationError
 	cmp			eax, LOWER_BOUND
-	jl			validationError
+	jl			invalid
 	cmp			eax, UPPER_BOUND
-	jg			validationError
-	jmp			calculateTerms
+	jg			invalid
+	jmp			valid
 
-validationError:
+invalid:
+	mov			ebx, 0
+	jmp			return
+
+valid:
+	mov			ebx, 1
+
+return:
+	ret
+
+validateInput	ENDP
+
+validationError		PROC
 
 	; Changed text color to lightRed (defined in Irvine32.inc) for error message output
 	mov			eax, lightRed
@@ -108,15 +153,108 @@ validationError:
 	; Reverting the text color back to the terminal's default after error prompt display
 	mov			eax, lightGray
 	call	SetTextColor
-	jmp			getNumOfCompTerms
+	ret
+validationError	ENDP
 
-calculateTerms:
+isComp		PROC
 
+	mov			ebx, compTerm
+	cmp			ebx, 3			; Checking if number are less than or equal to 3
+	jle			primeNum
+	mov			ecx, 3
+
+testDivide:
 	
+	mov			eax, ebx
+	xor			edx, edx
+	div			ecx
+	cmp			edx, 0
+	je			noRemainder
+	inc			ecx
+	cmp			ecx, compTerm
+	je			primeNum
+	jmp			testDivide
 
-	exit	; exit to operating system
-main ENDP
+noRemainder:
 
-; (insert additional procedures here)
+	mov			eax, 0
+	jmp			return
 
+primeNum:
+
+	mov			eax, 1
+
+return:
+
+	ret
+
+isComp	ENDP
+
+outputComposites	PROC
+	
+	mov			compTerm, 0
+	mov			eax, userInput
+	dec			eax
+	mov			userInputTmp, eax
+
+	; Initializing spaceCount with SPACE_COUNT
+	mov			eax, SPACE_COUNT
+	mov			spaceCount, eax
+
+calcComp:
+	
+	inc			compTerm
+	call	isComp
+	cmp			eax, 0
+	je			foundComp
+	jmp			calcComp
+
+foundComp:
+
+	mov			eax, compTerm
+	call	WriteDec
+	dec			spaceCount
+	jmp		formatting
+
+newRow:
+
+	call	CrLf
+	mov			eax, SPACE_COUNT
+	mov			spaceCount, eax
+	jmp			resume
+
+formatting:
+
+	mov			eax, countPH
+	cmp			eax, userInputTmp
+	je			newRow
+	mov			edx, OFFSET spaceChar
+	call	WriteString
+
+resume:
+
+	inc			countPH
+	mov			eax, countPH
+	cmp			eax, userInput
+	je			return
+	jmp			calcComp
+
+return:
+
+	call	CrLf
+
+	ret
+
+outputComposites	ENDP
+
+sayGoodbye	PROC
+	mov			edx, OFFSET certMsg
+	call	WriteString
+	call	CrLf
+	call	CrLf
+
+	ret
+
+sayGoodbye ENDP
+	
 END main
